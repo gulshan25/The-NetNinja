03c52156602df62d5a42a7f3e8982dce8d6b3739
gulshan25 <g.r.tanny@gmail.com>
2022-01-15 21:14:58 +0600

    changes

--- flutter/flutterapp/lib/main.dart
+++ flutter/flutterapp/lib/main.dart
@@ -1,1 +1,28 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Text(
+            'Hello, world!',
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.green[600],
+              fontFamily: 'IndieFlower',
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Hello'),
+          backgroundColor: Colors.green,
+        ),
+      ),
+    ));
